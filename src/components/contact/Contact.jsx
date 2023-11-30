a1cb1be81978bd041159d31ef9f81bccf49a0cb4
Dylan <dylankral@gmail.com>
2023-11-29 18:49:28 -0800

    added logo and contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,49 @@
 import React from "react";
 
+import logo from "../../assets/image2vector.svg";
+import "./Contact.css";
+
 function Contact() {
+  function sendEmail() {}
+
   return (
-    <div>
-      <p>Hey</p>
+    <div className="contactContainer">
+      <img id="contactLogo" src={logo} alt="Logo of the company" />
+
+      <div className="contactInfoContainer">
+        <form onSubmit={sendEmail}>
+          <input
+            type="text"
+            id="user_name"
+            name="user_name"
+            placeholder="Enter Name"
+          />
+          <br />
+          <input
+            type="email"
+            id="user_email"
+            name="user_email"
+            placeholder="Enter Email"
+          />
+          <br />
+          <input
+            type="number"
+            id="phone_number"
+            name="phone_number"
+            placeholder="Enter Phone Number"
+            maxLength="12"
+          />
+          <br />
+          <textarea
+            id="text-area"
+            name="message"
+            rows="4"
+            placeholder="Enter Message Here"
+          ></textarea>
+          <br />
+          <input type="submit" id="submit" value="Send" />
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import logo from "../../assets/image2vector.svg";
 import "./Nav.css";
 
 function Nav() {
   return (
     <nav>
       <ul id="navUl">
+        <div>
+          <img className="logo" id="elmPineLogo" src={logo} alt="Logo" />
+        </div>
         <li>
           <a href="" className="navlink">
             About
